7b7fa949cd1b97361808b42a8905e73937b5e40b
Alexandra2888 <moldovan.alexandra28@gmail.com>
2024-05-26 21:45:23 +0300

    tailwind fixes

--- app/(home)/components/Footer.tsx
+++ app/(home)/components/Footer.tsx
@@ -1,10 +1,10 @@
 import React from "react";
-import Navbar from "./Navbar";
+import Socials from "./Socials";
 
 export default function Footer() {
 	return (
-		<div className=" border-t mt-10">
-			<Navbar className="flex-col gap-5" />
+		<div className=" border-t mt-10 flex justify-center">
+			<Socials className="flex-col gap-5" />
 		</div>
 	);
 }
