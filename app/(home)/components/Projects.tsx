05e44d36bbfe8f0fe81f80f92799e553be4f591a
Alexandra28 <moldovan.alexandra28@gmail.com>
2024-08-11 16:23:04 +0300

    adding more projects

--- app/(home)/components/Projects.tsx
+++ app/(home)/components/Projects.tsx
@@ -1,24 +1,25 @@
 import React from "react";
 import {
-	SiCss3,
-	SiFirebase,
-	SiMongodb,
-	SiNextdotjs,
-	SiNodedotjs,
-	SiOpenai,
-	SiPrisma,
-	SiReact,
-	SiTailwindcss,
-	SiTypescript,
+  SiCss3,
+  SiFirebase,
+  SiMongodb,
+  SiNextdotjs,
+  SiNodedotjs,
+  SiOpenai,
+  SiPrisma,
+  SiReact,
+  SiStorybook,
+  SiTailwindcss,
+  SiTensorflow,
+  SiTypescript,
 } from "react-icons/si";
 import Title from "./Title";
 import Link from "next/link";
 import { cn } from "@/lib/utils";
 import { DirectionAwareHover } from "@/components/ui/direction-aware-hover";
-import { backIn } from "framer-motion";
 
 export default function Projects() {
-	const projects = [
+  const projects = [
     {
       title: "Pawsitive Vet Care",
       tech: [SiReact, SiTypescript, SiCss3, SiFirebase],
@@ -51,7 +52,7 @@ export default function Projects() {
       title: "WatchFlix",
       tech: [SiNextdotjs, SiTypescript, SiTailwindcss, SiPrisma, SiMongodb],
       link: "https://github.com/Alexandra2888/watchflix",
-      cover: "/watchflix.png",
+      cover: "/watchflix2.png",
       background: "bg-yellow-500",
     },
     {
@@ -61,49 +62,58 @@ export default function Projects() {
       cover: "/saas-ai.png",
       background: "bg-pink-500",
     },
+    {
+      title: "Object Detection",
+      tech: [SiNextdotjs, SiTailwindcss, SiTensorflow],
+      link: "https://github.com/Alexandra2888/Object-Detection",
+      cover: "/object-detection2.png",
+      background: "bg-blue-500",
+    },
+    {
+      title: "Component Craft-React UI Library",
+      tech: [SiNextdotjs, SiTailwindcss, SiStorybook],
+      link: "https://github.com/Alexandra2888/ComponentCraft",
+      cover: "/component-craft.png",
+      background: "bg-red-500",
+    },
   ];
 
-	return (
-		<div className="py-10 p-5 sm:p-0">
-			<Title
-				text="Projects 🎨"
-				className="flex flex-col items-center justify-center"
-			/>
-			<div className=" grid grid-cols-1 sm:grid-cols-2 pt-20 gap-5">
-				{projects.map((project, index) => {
-					return (
-						<Link href={project.link} key={index}>
-							<div
-								className={cn(
-									"p-5 rounded-md",
-									project.background
-								)}
-							>
-								<DirectionAwareHover
-									imageUrl={project.cover}
-									className="w-full space-y-5 cursor-pointer"
-								>
-									<div className=" space-y-5">
-										<h1 className="text-2xl font-bold">
-											{project.title}
-										</h1>
-										<div className="flex items-center gap-5">
-											{project.tech.map((Icon, index) => {
-												return (
-													<Icon
-														className="w-8 h-8"
-														key={index}
-													/>
-												);
-											})}
-										</div>
-									</div>
-								</DirectionAwareHover>
-							</div>
-						</Link>
-					);
-				})}
-			</div>
-		</div>
-	);
+  return (
+    <div className="py-10 p-5 sm:p-0">
+      <Title
+        text="Projects 🎨"
+        className="flex flex-col items-center justify-center"
+      />
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-5 pt-20">
+        {projects.map((project, index) => {
+          return (
+            <Link href={project.link} key={index}>
+              <div
+                className={cn(
+                  "p-5 rounded-md",
+                  project.background,
+                  "flex flex-col justify-between"
+                )}
+                style={{ height: "300px" }} 
+              >
+                <DirectionAwareHover
+                  imageUrl={project.cover}
+                  className="w-full h-full space-y-5 cursor-pointer flex flex-col justify-between"
+                >
+                  <div className="space-y-5">
+                    <h1 className="text-2xl font-bold">{project.title}</h1>
+                    <div className="flex items-center gap-5">
+                      {project.tech.map((Icon, index) => {
+                        return <Icon className="w-8 h-8" key={index} />;
+                      })}
+                    </div>
+                  </div>
+                </DirectionAwareHover>
+              </div>
+            </Link>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
